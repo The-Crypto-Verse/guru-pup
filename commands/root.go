0b1e4e17575a9c471fdde3fcef483a4da63c3d81
TheBoringDude <iamcoderx@gmail.com>
2022-04-04 08:14:30 +0800

    try to remove old commands first and fix guilds register

--- commands/root.go
+++ commands/root.go
@@ -3,6 +3,7 @@ package commands
 import (
 	"log"
 	"os"
+	"strings"
 
 	"github.com/TheBoringDude/minidis"
 	"github.com/bwmarrin/discordgo"
@@ -15,10 +16,17 @@ func Manager() *minidis.Minidis {
 	bot.SetIntents(discordgo.IntentsGuilds | discordgo.IntentsGuildMessages)
 
 	// sync only to specific servers
-	bot.SyncToGuilds(os.Getenv("GUILD"))
+	bot.SyncToGuilds(strings.Split(os.Getenv("GUILD"), ",")...)
 
 	bot.OnReady(func(s *discordgo.Session, i *discordgo.Ready) {
 		log.Println("Bot is ready!")
+	})
+
+	bot.OnBeforeStart(func(s *discordgo.Session) {
+		// try to remove the commands first to prevent duplicate commands
+		if err := bot.ClearCommands(); err != nil {
+			log.Fatal(err)
+		}
 	})
 
 	bot.OnClose(func(s *discordgo.Session) {
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.18
 
 require (
 	github.com/TheBoringDude/minidis v0.0.0-20220323140932-da42cd97f90d
+	github.com/World-of-Cryptopups/atomicassets-go v0.0.0-20220323074558-918cf916152e
 	github.com/World-of-Cryptopups/eosrpc.go v0.0.2-0.20220323031043-8dae71837407
 	github.com/bwmarrin/discordgo v0.24.1-0.20220307125103-9586f7fb38cd
 	github.com/deta/deta-go v1.0.0
@@ -11,7 +12,6 @@ require (
 
 require (
 	github.com/TheBoringDude/go-urljoin v0.0.1 // indirect
-	github.com/World-of-Cryptopups/atomicassets-go v0.0.0-20220323074558-918cf916152e // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064 // indirect
